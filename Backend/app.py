faf1b299ce354ed6f8a6cc262de59df4f4672ec5
AbhishekSaini9520 <abhisheksaini200404@gmail.com>
2026-05-16 20:44:34 +0530

    update the port number

--- Backend/app.py
+++ Backend/app.py
@@ -356,5 +356,6 @@ def dashboard():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
+    print(port)
     print(f"Starting server on port {port}")
     app.run(debug=False, host="0.0.0.0", port=port)
